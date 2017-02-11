a4c63209a83cb986d026fa10ae30d793380fb486
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-10 20:14:40 -0700

    remove head and tail functions added

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -30,4 +30,61 @@ LinkedList.prototype.addToHead = function (value) {
 
 myLinkedList.addToHead("First Node");
 myLinkedList.addToHead("Second Node");
-console.log(myLinkedList);
+myLinkedList.addToHead("Third Node");
+// console.log(myLinkedList);
+
+LinkedList.prototype.addToTail = function (value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  }
+  else {
+    this.head =  newNode;
+  }
+  this.tail = newNode;
+};
+
+myLinkedList.addToTail("Fourth Node");
+myLinkedList.addToTail("Fifth Node");
+myLinkedList.addToTail("Sixth Node");
+// console.log(myLinkedList);
+
+LinkedList.prototype.removeHead = function () {
+  if(!this.head) {
+    return null;
+  }
+  var val = this.head.value;
+  this.head = this.head.next;
+  if (this.head) {
+    this.head.prev = null;
+  }
+  else {
+    this.tail = null;
+  }
+  return val;
+};
+
+// console.log(myLinkedList);
+// myLinkedList.removeHead();
+// console.log(myLinkedList);
+// myLinkedList.removeHead();
+// console.log(myLinkedList);
+
+LinkedList.prototype.removeTail = function() {
+  if(!this.tail) {
+    return null;
+  }
+  var val = this.tail.value;
+  this.tail = this.tail.prev;
+  if(this.tail) {
+    this.tail.next = null;
+  }
+  else {
+    this.head = null;
+  }
+  return val;
+};
+
+// console.log(myLinkedList);
+// myLinkedList.removeTail();
+// console.log(myLinkedList);
